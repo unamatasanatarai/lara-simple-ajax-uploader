8ff1b161eb64dc9e4208594501dbdff656311a8c
piotr grabowski <piotr.grabowski@performancehorizon.com>
2017-03-17 11:13:23 +0100

    qf

--- src/Unamatasanatarai/SimpleAjaxUploader/resources/views/inline.blade.php
+++ src/Unamatasanatarai/SimpleAjaxUploader/resources/views/inline.blade.php
@@ -1,28 +1,17 @@
-<?php
-$label = $attributes['label'] ?? '';
-$url = $attributes['url'] ?? '';
-
-$attributes['type'] = 'hidden';
-$attributes['class'] = 'filepath';
-
-
-unset($attributes['url']);
-unset($attributes['label']);
-
-?>
-<div class="form-group sau-upload sau-upload-inline" data-url="{{ $url }}">
+<div class="form-group sau-upload sau-upload-inline" data-url="{{ $url or '' }}">
     @if(!empty($label))
-        <label for="{{ $id }}">{{ $label }}</label>
+        <label>{{ $label }}</label>
     @endif
     <div class="sau-dropzone sau-inputs" id="sau_dropzone_{{ rand(1, 5000) . uniqid() }}">
         <div class="input-group">
-            <input type="text" value="{{ $attributes['displayName'] or '' }}" class="sau-displayname form-control" readonly>
+            <input type="text" value="{{ $displayName or '' }}" class="sau-displayname form-control" readonly>
             <span class="input-group-btn">
                 <button class="sau-button" id="sau_button_{{ rand(1, 5000) . uniqid() }}">{{ $buttonCaption or 'Select file' }}</button>
             </span>
 
-        <div class="sau-progress progress">
-            <div class="determinate"></div>
+            <div class="sau-progress progress">
+                <div class="determinate"></div>
+            </div>
         </div>
     </div>
 
