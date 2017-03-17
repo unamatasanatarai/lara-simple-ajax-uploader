d4144ce481aaf6fd787ec39fea8d45b7ad66e118
Piotr Grabowski <piograski@gmail.com>
2017-03-17 18:30:49 +0100

    qf

--- src/Unamatasanatarai/SimpleAjaxUploader/resources/views/images_item.blade.php
+++ src/Unamatasanatarai/SimpleAjaxUploader/resources/views/images_item.blade.php
@@ -1,5 +1,5 @@
 <div class="sau-item" data-name="{{ $name }}">
-    <span class="listendelete">remove</span>
+    <span class="listendelete">{{ $removeText }}</span>
     <input type="hidden" class="sau-filepath" name="{{ $name }}[]" value="{{ $src }}">
     <img src="{{ $url }}" class="hide">
     <div class="loading"></div>
